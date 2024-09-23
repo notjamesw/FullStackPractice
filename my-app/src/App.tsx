38f8911491b215e4b43d4564f4d627d24912491b
James Wen <leranjamesw@gmail.com>
2024-09-23 15:29:39 -0700

    implemented simple terminal

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState , useEffect, useRef} from 'react';
+
+function Terminal() {
+    const [input, setInput] = useState<string>('');
+    const [output, setOutput] = useState<string[]>([]);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+        processCommand(input);
+        setInput("");
+    }
+  }
+
+  const processCommand = (command: string) => {
+    let prevOutput: string[];
+
+    if (output.length > 0) {
+      prevOutput = output;
+    } else {
+      prevOutput = [];
+    }
+
+    setOutput([...prevOutput, `> ${command}`]);
+  }
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  return(
+    <div className = "Terminal">
+      <div className = "Output">
+        {output.map((line, index) => (
+          <div key={index}>{line}</div>
+        ))}
+      </div>
+      <div className = "Input">
+        <span>{'> '}</span>
+        <input
+          ref = {inputRef}
+          value = {input}
+          onChange={e=>setInput(e.target.value)}
+          onKeyDown={handleInput}
+          autoFocus
+        />
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Racket Interpreter
       </header>
+      <Terminal/>
     </div>
   );
 }
