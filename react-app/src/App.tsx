59561e40b87fc5f454c5050bcdb7f3ebc183235b
jameswen <leranjamesw@gmail.com>
2024-10-04 16:33:15 -0700

    added axios for http requests

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import './App.css';
 import { useState , useEffect, useRef} from 'react';
+import axios from 'axios';
+
+async function runInterpreter(input: string): Promise<string> {
+  let output: string = "";
+  try {
+    const response = await axios.post('http://127.0.0.1:5000/run_interpreter', {
+      input: input
+    });
+    output = response.data.output;
+  } catch(err) {
+    console.error("Interpreter error processing input:", err);
+  }
+  return output;
+};
 
 function Terminal() {
   const [input, setInput] = useState<string>("");
   const [output, setOutput] = useState<string[]>([]);
   const inputRef = useRef<HTMLInputElement>(null);
   const [pathPrefix, setPathPrefix] = useState<string>("> ");
+  let isProgramActive: boolean = false;
 
   const handleInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
         processCommand(input);
         setInput("");
+    } else if (e.key === "ArrowUp") { // endpoint
+        processCommand(input);
     }
   }
 
   // this allows the app to show previous input/outputs
-  const processCommand = (command: string) => {
+  const processCommand = async (command: string) => {
     let prevOutput: string[];
 
     if (output.length > 0) {
@@ -25,7 +42,34 @@ function Terminal() {
       prevOutput = [];
     }
 
-    setOutput([...prevOutput, pathPrefix + command, command + "operation"]);
+    // check if interpreter programs is running
+    if (isProgramActive) {
+      // command to quit the interpreter if it is running
+      if (command.toLowerCase() === "quit") {
+        setPathPrefix("> ");
+        isProgramActive = false;
+        return;
+      }
+    } else { 
+      // command to start the interpreter if it is not running    
+      if (command.toLowerCase() === "lisp" || command.toLowerCase() === "racket") {
+        isProgramActive = true;
+        setPathPrefix("Racket> "); 
+        return;
+      }
+    }
+    
+    if (command.toLowerCase() === "clear") {
+      setOutput([]);
+      return;
+    } else {
+      // run interpreter
+      let programOutput: string = pathPrefix + command; 
+      if (isProgramActive) {
+        programOutput = await runInterpreter(command);
+      }
+      setOutput([...prevOutput, pathPrefix + command, programOutput]);
+    }
   }
 
   useEffect(() => {
